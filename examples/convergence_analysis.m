3aa311d292c9c004e0cc5b9dd8ca986ce0770fbc
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2025-02-15 23:55:09 -0300

    ENH: L2 convergence analysis in place

--- examples/convergence_analysis.m
+++ examples/convergence_analysis.m
@@ -2,26 +2,55 @@
 % to the Darcy problem with unit permeability, using a trigonometric
 % manufactured solution
 
-ncells = [8, 16, 32, 64];
-k = [2];
+ncells = [16; 32; 64; 128];
 
-errors_k2_pcenter = zeros(length(ncells), 1);
-#errors_k4_p = zeros(length(ncells), 1);
-errors_k2_pnodes = zeros(length(ncells), 1);
-#errors_k4_q = zeros(length(ncells), 1);
+errors_p_cell_centers = zeros(length(ncells), 1);
+errors_p_edge_centers = zeros(length(ncells), 1);
+errors_p_nodes = zeros(length(ncells), 1);
+errors_q_edge_centers = zeros(length(ncells), 1);
 
-for ii=1:length(k)
-    for jj=1:length(ncells)
-
-        [error_pcenter, error_pnodes] = darcy_unit_perm_model(ncells(jj), k(ii));
-
-        if k(ii) == 2
-            errors_k2_pcenter(jj) = error_pcenter;
-            errors_k2_pnodes(jj) = error_pnodes;
-        else
-            errors_k4_p(jj) = error_p;
-            errors_k4_q(jj) = error_q;
-        end
-    end
+for ii=1:length(ncells)
+    errors = darcy_unit_perm_model(ncells(ii), 2);
+    errors_p_cell_centers(ii) = errors.p_cell_centers;
+    errors_p_edge_centers(ii) = errors.p_edge_centers;
+    errors_p_nodes(ii) = errors.p_nodes;
+    errors_q_edge_centers(ii) = errors.q_edge_centers;
 end
 
+% Compute log values for plotting
+log_ncells = log2(ncells);
+log_errors_p_cell_centers = log2(errors_p_cell_centers);
+log_errors_p_edge_centers = log2(errors_p_edge_centers);
+log_errors_p_nodes = log2(errors_p_nodes);
+log_errors_q_edge_centers = log2(errors_q_edge_centers);
+
+% Plot errors
+figure();
+plot(log_ncells, log_errors_p_cell_centers, '-or', 'LineWidth', 1.5);
+hold on;
+plot(log_ncells, log_errors_p_edge_centers, '-ob', 'LineWidth', 1.5);
+plot(log_ncells, log_errors_p_nodes, '-og', 'LineWidth', 1.5);
+plot(log_ncells, log_errors_q_edge_centers, '-om', 'LineWidth', 1.5);
+
+% -----> Choose your custom first y-point for the second-order reference slope
+y_start = -10;  % Change this to any desired starting y-point
+
+% Define the second-order slope (-2) while keeping the full x-range
+x_ref = [log_ncells(1), log_ncells(end)];
+y_ref = [y_start, y_start - 2 * (log_ncells(end) - log_ncells(1))];
+
+plot(x_ref, y_ref, '--k', 'LineWidth', 1.5); % Dashed black line for order 2 slope
+
+% Labels and legend
+xlabel('log2(Number of cells)', 'FontSize', 15);
+ylabel('log2(Discrete relative L2-error)', 'FontSize', 15);
+
+legend({'Pressure (cell-center)', ...
+        'Pressure (edge-center)', ...
+        'Pressure (node)', ...
+        'Flux (edge-center)', ...
+        'Reference slope 2'}, 'Location', 'Best', 'FontSize', 15);
+
+grid on;
+hold off;
+print('convergence.png', '-depsc');
